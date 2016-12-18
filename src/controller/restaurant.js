07178f854d31e775e75a8d68193ce76552fe3765
rath-prak <rathanap@gmail.com>
2016-12-18 16:37:43 +1300

    set up put and delete

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -5,6 +5,8 @@ import Restaurant from '../model/restaurant';
 export default({ config, db }) => {
   let api = Router();
 
+  // CRUD -  read, create, update, delete
+
   // '/v1/restaurant/add'
   api.post('/add', (req, res) => {
     let newRest = new Restaurant();
@@ -17,6 +19,46 @@ export default({ config, db }) => {
       res.json({ message: 'Restaurant saved sucessfully!'});
     });
   });
+
+  // '/v1/restaurant - read'
+
+  api.get('/', (req, res) => {
+      Restaurant.find({}, (err, restaurants) => {
+        if(err) {
+          res.send(err);
+        }
+        res.json(restaurants);
+      });
+  });
+
+  // '/v1/restaurant/:id - read id only
+  api.get('/:id', function (req, res){
+    Restaurant.findById(req.params.id, function(err, restaurant){
+      if(err) {
+        res.send(err);
+      }
+      res.json(restaurant);
+    });
+  });
+
+  // '/v1/restaurant/:id - update
+
+  api.put('/:id', function (req, res) {
+    Restaurant.findById(req.params.id, function(err, restaurant){
+      if(err) {
+        res.send(err);
+      }
+      restaurant.name = req.body.name;
+      restaurant.save(function(err){
+        if (err) {
+          res.send(err);
+        }
+        res.json({ message: "restaurant has been updated"});
+      })
+    });
+  });
+
+
 
   return api;
 }
